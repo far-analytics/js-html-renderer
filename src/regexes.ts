446e6b55610578547ed42dadd589151bcd75c64a
Adam Patterson <adpatter2000@gmail.com>
2025-12-31 13:57:00 -0500

    Format.

--- src/regexes.ts
+++ src/regexes.ts
@@ -1,5 +1,5 @@
-// eslint-disable-next-line no-control-regex
 export const attributeName =
+  // eslint-disable-next-line no-control-regex
   /^[^\u{0000}-\u{001f}\u{0022}\u{0027}\u{003e}\u{002f}\u{003d}\u{e000}-\u{f8ff}\u{f0000}-\u{ffffd}]+$/u;
 // eslint-disable-next-line no-control-regex
 export const attributeValue = /^[^\u{0000}-\u{0008}\u{000b}\u{000e}-\u{001F}\u{e000}-\u{f8ff}\u{f0000}-\u{ffffd}]*$/u;
